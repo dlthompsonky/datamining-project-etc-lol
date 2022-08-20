be142d123ba1d3542fa54f88755f0c70334f1691
MyNameDave <dave88893@gmail.com>
2022-08-20 12:03:36 -0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,23 @@
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+from riotwatcher import RiotWatcher, LolWatcher, ApiError #Importing Riotwatcher & other (Riot API)
+
+lolWatcher_api_key = LolWatcher('RGAPI-704ce981-8404-4b57-bf27-21e702fed59b')
+region = 'na1'
+
+
+me = lolWatcher_api_key.summoner.by_name(region, 'Sobileo')
+
+print(me)
+print(lolWatcher_api_key.league.)
+
+
+
+
+
+
+
+
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
