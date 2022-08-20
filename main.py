44e69c57827f3efbfb66d78b9abef539e259d42a
MyNameDave <dave88893@gmail.com>
2022-08-20 13:44:26 -0500

    Match History IDs
    This update includes the match history IDs collected from the riot accounts put into the list.

--- main.py
+++ main.py
@@ -15,15 +15,23 @@ list_of_summoners = [lolWatcher_api_key.summoner.by_name(region, 'Sobileo'),
                      lolWatcher_api_key.summoner.by_name(region, 'Airpiane'),
                      lolWatcher_api_key.summoner.by_name(region, 'Ominous Cow')]
 
-list_of_account_IDS = []
+list_of_puuids = []
+
+
+#Need to make sure to put this all within try function in order to keep errors minimmal
 
 
 def printListOfSummoners(): #Prints list of the summoner objects (This includes a lot of information)
     for x_summoner in list_of_summoners:
         print(x_summoner)
-    for x_summoner_id in list_of_summoners:
-        print(x_summoner_id['accountId']) #This is to collect the accountId of each account
+    for x_puuid_id in list_of_summoners:
+        print(x_puuid_id['puuid']) #This is to collect the accountId of each account
+        list_of_puuids.append(x_puuid_id['puuid'])
+    for x_puuid in list_of_puuids:
+        print(lolWatcher_api_key.match.matchlist_by_puuid(region, x_puuid))
 
+
+print(lolWatcher_api_key.match.matchlist_by_puuid(region, "4fBgyUfYU31uJiwccVHEN69Dull5MJ_TV_5ykLQiVhdjTnZsxWJdrUOYtuL6Tbrz9F3MB_VDN7y8JA"))
 
 printListOfSummoners()
 
